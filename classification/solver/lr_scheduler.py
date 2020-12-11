024aeb14e8a586300638d18c8343d0c94d64f22a
xiaochengsky <626576619@qq.com>
2020-12-11 23:29:10 +0800

    lr_scheduler

---
+++ classification/solver/lr_scheduler.py
@@ -0,0 +1,22 @@
+
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.optim.lr_scheduler import *
+
+
+class wrapper_lr_scheduler(object):
+    def __init__(self, cfg_lr_scheduler, optimizer):
+        cfg_lr_scheduler_c = cfg_lr_scheduler.copy()
+        lr_scheduler_type = cfg_lr_scheduler_c.pop("type")
+        if hasattr(lr_scheduler, lr_scheduler_type):
+            self.lr = getattr(lr_scheduler, lr_scheduler_type)(optimizer, **cfg_lr_scheduler_c)
+        else:
+            raise KeyError("lr_scheduler{} not found!!!".format(lr_scheduler_type))
+
+    def ITERATION_COMPLETED(self):
+        if isinstance(self.lr, (CyclicLR, OneCycleLR, ExponentialLR)):
+            self.lr.step()
+
+    def EPOCH_COMPLETED(self):
+        if isinstance(self.lr, (StepLR, MultiStepLR, CosineAnnealingLR, CosineAnnealingWarmRestarts)):
+            self.lr.step()
+
