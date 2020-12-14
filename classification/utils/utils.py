1e10359856ecc7b8d40032fb2e72f80fb8e3fd1b
xiaochengsky <626576619@qq.com>
2020-12-15 00:08:03 +0800

    add cnf_matrix

--- classification/utils/utils.py
+++ classification/utils/utils.py
@@ -4,6 +4,9 @@ import os
 import time
 from contextlib import contextmanager
 from copy import deepcopy
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
 
 import torch
 import torch.backends.cudnn as cudnn
@@ -97,4 +100,52 @@ def weights_init_kaiming(m):
         if m.affine:
             nn.init.normal_(m.weight, 1.0, 0.02)
             nn.init.constant_(m.bias, 0.0)
+
+
+def get_cnf_matrix(pred_logit, target):
+    indics = torch.max(pred_logit, dim=1)[1]
+    p_t = np.zeros((5, 5))
+    for i in range(0, 5):
+        for j in range(0, 5):
+            p_t[i][j] = torch.sum((indics == i) & (target == j)).item()
+
+    return p_t
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    # print(cm)
+
+    fig = plt.figure()
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('Predicted label')
+    plt.xlabel('True label')
+    return fig
 
