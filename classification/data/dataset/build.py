f0b4b4c62dfda80779f16aae30219c44190952f3
xiaochengsky <626576619@qq.com>
2020-12-17 08:21:15 +0800

    add query and gallery interface.

--- classification/data/dataset/build.py
+++ classification/data/dataset/build.py
@@ -26,10 +26,8 @@ class load_images(Dataset):
     def __getitem__(self, idx):
         image_name = self.image_list[idx]
         image = cv2.imread(os.path.join(self.image_dir, image_name))
-        sample = {'image': image}
         if self.transforms:
-            sample = self.transforms(sample)
-        image = sample['image']
+            image = self.transforms(image)
         return image, image_name
 
 
@@ -181,4 +179,68 @@ class test_dataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image, image_name
+
+
+class gallery_dataset(Dataset):
+    def __init__(self, root_dir, label_path, transforms):
+        self.root_dir = root_dir
+        self.transform = transforms
+        self.labels = []
+        self.datas = []
+        # label_name = 'gallery_labels.txt'
+        label_name = label_path
+
+        file = open(os.path.join(root_dir, label_name))
+        while True:
+            word = file.readline()
+            if not word:
+                break
+            word = word.split(',')
+            label = int(word[1])
+            image_name = word[0]
+            self.datas.append(image_name)
+            self.labels.append(label)
+        file.close()
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        image_name, target = self.datas[idx], self.labels[idx]
+        image = cv2.imread(os.path.join(self.root_dir, image_name))
+        if self.transform:
+            image = self.transform(image)
+        return image, target, image_name
+
+
+class query_dataset(Dataset):
+    def __init__(self, root_dir, label_path, transforms):
+        self.root_dir = root_dir
+        self.transform = transforms
+        self.labels = []
+        self.datas = []
+        # label_name = 'query_labels.txt'
+        label_name = label_path
+
+        file = open(os.path.join(root_dir, label_name))
+        while True:
+            word = file.readline()
+            if not word:
+                break
+            word = word.split(',')
+            label = int(word[1])
+            image_name = word[0]
+            self.datas.append(image_name)
+            self.labels.append(label)
+        file.close()
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        image_name, target = self.datas[idx], self.labels[idx]
+        image = cv2.imread(os.path.join(self.root_dir, image_name))
+        if self.transform:
+            image = self.transform(image)
+        return image, target, image_name
 
