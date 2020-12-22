d898401f143e9e74375cf78a8cee89c1bde21bb8
xiaochengsky <626576619@qq.com>
2020-12-22 23:33:26 +0800

    add sampler

--- classification/data/dataloader/build.py
+++ classification/data/dataloader/build.py
@@ -1,8 +1,19 @@
 from ..dataset import build_dataset
 from ..transforms.build import build_transforms
-# import .build_dataset()
-
+# import .build_dataset
+from . import sampler as sampler
 from torch.utils.data import DataLoader
+
+
+def build_sampler(cfg_sampler, dataset):
+    cfg_sampler_c = cfg_sampler.copy()
+    sampler_type = cfg_sampler_c.pop("type")
+    sampler_params = cfg_sampler_c
+    if hasattr(sampler, sampler_type):
+        dataset_sampler = getattr(sampler, sampler_type)(dataset)
+    else:
+        raise ValueError("\'type\'{} of dataset is not defined!!!".format(sampler_type))
+    return dataset_sampler
 
 
 def create_dataloader(cfg_data_pipeline):
@@ -13,6 +24,11 @@ def create_dataloader(cfg_data_pipeline):
 
     transforms = build_transforms(cfg_transforms)
     dataset = build_dataset(cfg_dataset, transforms)
+
+    if "sampler" in cfg_data_pipeline_c:
+        cfg_sampler = cfg_data_pipeline_c.pop("sampler")
+        dataset_sampler = build_sampler(cfg_sampler, dataset)
+        print("sampler")
 
     if "sampler" in cfg_dataloader:
         cfg_sampler = cfg_dataloader.pop("sampler")
@@ -26,5 +42,10 @@ def create_dataloader(cfg_data_pipeline):
                 return dataloader
         else:
             print("use pytorch collate_fn")
-            dataloader = DataLoader(dataset, **cfg_dataloader)
+            if "sampler" in cfg_data_pipeline:
+                print('use owner sampler')
+                dataloader = DataLoader(dataset, sampler=dataset_sampler, **cfg_dataloader)
+            else:
+                dataloader = DataLoader(dataset, **cfg_dataloader)
             return dataloader
+
