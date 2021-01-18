b53192f1c2dc6b6f47f36dda8a0648e9def38294
xiaochengsky <626576619@qq.com>
2021-01-19 00:09:49 +0800

    添加 snapmixloss 和 RGA 模块

--- classification/model/nets/__init__.py
+++ classification/model/nets/__init__.py
@@ -1,2 +1,5 @@
 from .ResNet50_GeM_Identity_CE import *
 from .ResNet50_GeM_Identity_FocalLoss import *
+from .ResNet50_GeM_Identity_SnapMix_CE import *
+from .ResNet50_RGA_GAP_Identity_SnapMix_CE import *
+
