81892895dcc9e09314e58c5a517ffdd117dcd199
xiaochengsky <626576619@qq.com>
2021-01-19 00:11:24 +0800

    分离 fineturn 和其它部分的学习率变化策略

--- classification/solver/optimizer.py
+++ classification/solver/optimizer.py
@@ -1,4 +1,3 @@
-
 from torch import optim
 
 
@@ -8,9 +7,43 @@ def create_optimizer(cfg_optimizer, model):
     if hasattr(optim, optimizer_type):
         # 获取模型所有参数
         params = model.parameters()
-        optimizer = getattr(optim, optimizer_type)(params, **cfg_optimizer_c)
+        print(cfg_optimizer_c)
+
+        param_type = []
+        param_groups = []
+        # 为不同的层设置不同的 lr
+        for name, param in model.named_parameters():
+            if 'backbone' in name and 'backbone' not in param_type:
+                param_type.append('backbone')
+                param_groups.append({'params': model.backbone.parameters(), 'lr': cfg_optimizer_c['lr'] * 0.1})
+            if 'layer' in name and 'layer' not in param_type:
+                param_type.append('layer')
+                param_groups.append({'params': model.layer.parameters(), 'lr': cfg_optimizer_c['lr']})
+            if 'aggregation' in name and 'aggregation' not in param_type:
+                param_type.append('aggregation')
+                param_groups.append({'params': model.aggregation.parameters(), 'lr': cfg_optimizer_c['lr']})
+            if 'celoss' in name and 'celoss' not in param_type:
+                param_type.append('celoss')
+                param_groups.append({'params': model.celoss.parameters(), 'lr': cfg_optimizer_c['lr']})
+
+        print(param_type)
+        print(param_groups)
+        # param_groups = [
+        #    {'params': model.backbone.parameters(), 'lr': cfg_optimizer_c['lr'] * 0.1},
+        #    {'params': model.celoss.parameters(), 'lr': cfg_optimizer_c['lr']},
+        # ]
+        optimizer = getattr(optim, optimizer_type)(param_groups, **cfg_optimizer_c)
+        lr = []
+        i = 0
+        for param_group in optimizer.param_groups:
+            if i == 0:
+                print('backbone')
+            else:
+                print('else')
+            lr += [param_group['lr']]
+            i += 1
+        print(lr)
+
         return optimizer
     else:
         raise KeyError("optimizer{} is not found!!!".format(optimizer_type))
-
-
