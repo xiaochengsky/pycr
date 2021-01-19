d7876988339f301dd29e8c02ab5b26e80646a56e
xiaochengsky <626576619@qq.com>
2021-01-19 23:50:20 +0800

    opencv -> PIL

--- classification/data/dataset/build.py
+++ classification/data/dataset/build.py
@@ -211,7 +211,8 @@ class gallery_dataset(Dataset):
 
     def __getitem__(self, idx):
         image_name, target = self.datas[idx], self.labels[idx]
-        image = cv2.imread(os.path.join(self.root_dir, image_name))
+        # image = cv2.imread(os.path.join(self.root_dir, image_name))
+        image = Image.open(os.path.join(self.root_dir, image_name))
         if self.transform:
             image = self.transform(image)
         return image, target, image_name
@@ -243,7 +244,8 @@ class query_dataset(Dataset):
 
     def __getitem__(self, idx):
         image_name, target = self.datas[idx], self.labels[idx]
-        image = cv2.imread(os.path.join(self.root_dir, image_name))
+        # image = cv2.imread(os.path.join(self.root_dir, image_name))
+        image = Image.open(os.path.join(self.root_dir, image_name))
         if self.transform:
             image = self.transform(image)
         return image, target, image_name
