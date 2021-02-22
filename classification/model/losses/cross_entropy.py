7f85d4e233d4c2f697baa0444a5d7cc82030fa91
xiaochengsky <626576619@qq.com>
2021-02-22 17:48:55 +0800

    ce

--- classification/model/losses/cross_entropy.py
+++ classification/model/losses/cross_entropy.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 # 合并 FC + CE
 class CrossEntroy(nn.Module):
-    def __init__(self, in_feat=2048, num_classes=5, weight=1.0):
+    def __init__(self, in_feat=512, num_classes=5, weight=1.0):
         super(CrossEntroy, self).__init__()
         self.linear = nn.Linear(in_feat, num_classes, bias=False)
         self.criterion = nn.CrossEntropyLoss()
@@ -22,45 +22,40 @@ class CrossEntroy(nn.Module):
             return predicts
 
 
-# 合并 FC + CE +
-class CrossEntroyLabelSmoothing(nn.Module):
+
+class LabelSmoothCrossEntroy(nn.Module):
     def __init__(self, in_feat=2048, num_classes=5, lb_smooth=0.1, reduction='mean', ignore_index=-100, weight=1.0):
-        super(CrossEntroyLabelSmoothing, self).__init__()
-        self.lbs = lb_smooth
-        self.num_classes = num_classes
-        self.reduction = reduction
+        super(LabelSmoothCrossEntroy, self).__init__()
         self.lb_smooth = lb_smooth
+        self.reduction = reduction
+        self.num_classes = num_classes
         self.lb_ignore = ignore_index
         self.log_softmax = nn.LogSoftmax(dim=1)
         self.linear = nn.Linear(in_feat, num_classes, bias=False)
+        self.criterion = nn.CrossEntropyLoss()
         self.weight = weight
 
     def forward(self, predicts, targets=None):
-        '''
-        args: predicts: tensor of shape (N, C)
-        args: targets: tensor of shape(N.)
-        '''
         predicts = self.linear(predicts)
-        # overcome ignored label
 
+        # 训练模式, 输出 CE loss
         if self.training:
+            predicts = predicts.float()
             with torch.no_grad():
                 targets = targets.clone().detach()
-                ignore = targets.eq(self.lb_ignore)
-                n_valid = ignore.eq(0).sum()
-                targets[ignore] = 0
                 lb_pos, lb_neg = 1. - self.lb_smooth, self.lb_smooth / self.num_classes
                 lb_one_hot = torch.empty_like(predicts).fill_(lb_neg).scatter_(1, targets.unsqueeze(1), lb_pos).detach()
 
             logs = self.log_softmax(predicts)
             loss = -torch.sum(logs * lb_one_hot, dim=1)
-            loss[ignore] = 0
             if self.reduction == 'mean':
-                loss = loss.sum() / n_valid
+                loss = loss.mean()
             if self.reduction == 'sum':
                 loss = loss.sum()
+            return loss * self.weight
 
-            return loss
-
+        # 测试模式, 由 FC 输出
         else:
             return predicts
+
+
