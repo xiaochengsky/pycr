35ceca46ce987555d30e052c00524a8f8c1796e8
xiaochengsky <626576619@qq.com>
2021-02-22 17:52:16 +0800

    some example of config a net

--- classification/model/nets/__init__.py
+++ classification/model/nets/__init__.py
@@ -2,4 +2,15 @@ from .ResNet50_GeM_Identity_CE import *
 from .ResNet50_GeM_Identity_FocalLoss import *
 from .ResNet50_GeM_Identity_SnapMix_CE import *
 from .ResNet50_RGA_GAP_Identity_SnapMix_CE import *
+from .ResNet50_RGA4_GAP_Identity_SnapMix_CE import *
+from .EfficientNets_GAP_Identity_SnapMix_CE import *
+from .EfficientNets_GAP_Identity_CE import *
+from .TIMM_EfficientNets_GAP_Identity_CE import *
+from .TIMM_EfficientNets_GAP_Identity_SnapMix_CE import *
+from .TIMM_EfficientNets_RGA_GAP_Identity_SnapMix_CE import *
+from .SEResNets import *
+from .TIMM_ResNeSt_GAP_Identity_SnapMix_CE import *
+from .ResNet_GAP_Identity_SnapMix_Arcface import *
+from .TIMM_EfficientNets_GAP_BNN_Tri_Arc import *
+
 
