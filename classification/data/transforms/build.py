728d1bb7dc84da02ccd22084a06b83c2cd529ad4
xiaochengsky <626576619@qq.com>
2021-02-22 17:39:26 +0800

    build transforms

--- classification/data/transforms/build.py
+++ classification/data/transforms/build.py
@@ -1,4 +1,5 @@
-from . import cv2_transforms as transforms
+# from . import cv2_transforms as transforms
+# import torchvision.transforms as transforms
 
 
 def build_transforms(cfg_transforms):
@@ -6,17 +7,24 @@ def build_transforms(cfg_transforms):
     transforms_list = list()
     for item in cfg_transforms:
         transforms_type = item.pop("type")
-        transforms_params = item
-        # for debug
-        # print(transforms_params)
-        if hasattr(transforms, transforms_type):
-            # for debug
-            # print('transforms: ', transforms)
-            # print(getattr(transforms, transforms_type))
-            transforms_list.append(getattr(transforms, transforms_type)(**transforms_params))
-        else:
-            raise ValueError("\'type\'{} of transforms is not defined!!!".format(transforms_type))
 
-    # for debug
+        if transforms_type == 'transforms_type':
+            if item['backend'] == 'cv2':
+                from . import cv2_transforms as transforms
+            else:
+                # import torchvision.transforms as transforms
+                from . import transforms as transforms
+        else:
+            transforms_params = item
+            # for debug
+            # print(transforms_params)
+            if hasattr(transforms, transforms_type):
+                # for debug
+                # print('transforms: ', transforms)
+                # print(getattr(transforms, transforms_type))
+                transforms_list.append(getattr(transforms, transforms_type)(**transforms_params))
+            else:
+                raise ValueError("\'type\'{} of transforms is not defined!!!".format(transforms_type))
+
     # print(transforms_list)
     return transforms.Compose(transforms_list)
