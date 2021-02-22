a5122b65b34e34a041419a72beb6e1b5742aa080
xiaochengsky <626576619@qq.com>
2021-02-22 17:49:32 +0800

    汇总 SEResNet 的loos

---
+++ classification/model/losses/SEResNets_loss.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+
+class SEResNeXt_SnapMixLoss(nn.Module):
+    def __init__(self, in_feat=2048, num_classes=5, weight=1.0):
+        super(SEResNeXt_SnapMixLoss, self).__init__()
+        self.linear = nn.Linear(in_feat, num_classes)
+        self.criterion = nn.CrossEntropyLoss(reduction='none').cuda()
+        self.weight = weight
+
+    # def forward(self, criterion, outputs, ya, yb, lam_a, lam_b):
+    #     loss_a = criterion(outputs, ya)
+    #     loss_b = criterion(outputs, yb)
+    #     loss = torch.mean(loss_a * lam_a + loss_b * lam_b)
+    #     return loss
+
+    def forward(self, predicts, ya, yb, lam_a, lam_b, reback=None):
+        predicts = self.linear(predicts)
+
+        # if self.training:
+        #     loss_a = self.criterion(predicts, ya)
+        #     loss_b = self.criterion(predicts, yb)
+        #     loss = torch.mean(loss_a * lam_a + loss_b * lam_b)
+        #     return loss * self.weight
+
+        if self.training:
+            if reback is None:
+                loss_a = self.criterion(predicts, ya)
+                loss_b = self.criterion(predicts, yb)
+                loss = torch.mean(loss_a * lam_a + loss_b * lam_b)
+                # return loss * self.weight
+                return loss
+            elif reback == 'eval_cls_backbone':
+                return predicts
+
+        else:
+            return predicts
+
