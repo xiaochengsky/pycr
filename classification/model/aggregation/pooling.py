79f363bfc088824cf65ffc59c9c342d8dd5ab30e
xiaochengsky <626576619@qq.com>
2021-02-22 17:45:19 +0800

    add soft pooling

--- classification/model/aggregation/pooling.py
+++ classification/model/aggregation/pooling.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
+from torch.nn.modules.utils import _triple, _pair, _single
 
 
 class Flatten(nn.Module):
@@ -73,5 +74,36 @@ class AdaptiveAvgMaxPool2d(nn.Module):
         x_max = F.adaptive_max_pool2d(x, 1)
         x = x_max + x_avg
         return x
+
+
+def soft_pool2d(x, kernel_size=None, stride=None, force_inplace=False):
+    # Get input sizes
+    _, c, h, w = x.size()
+    if kernel_size is None:
+        kernel_size = _pair(h)
+    else:
+        kernel_size = _pair(kernel_size)
+    if stride is None:
+        stride = kernel_size
+    else:
+        stride = _pair(stride)
+
+    # Create per-element exponential value sum : Tensor [b x 1 x h x w]
+    e_x = torch.sum(torch.exp(x), dim=1, keepdim=True)
+    # Apply mask to input and pool and calculate the exponential sum
+    # Tensor: [b x c x h x w] -> [b x c x h' x w']
+    return F.avg_pool2d(x.mul(e_x), kernel_size, stride=stride).mul_(sum(kernel_size)).div_(
+        F.avg_pool2d(e_x, kernel_size, stride=stride).mul_(sum(kernel_size)))
+
+
+class SoftPool2d(torch.nn.Module):
+    def __init__(self, kernel_size=None, stride=None, force_inplace=False):
+        super(SoftPool2d, self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.force_inplace = force_inplace
+
+    def forward(self, x):
+        return soft_pool2d(x, kernel_size=self.kernel_size, stride=self.stride, force_inplace=self.force_inplace)
 
 
